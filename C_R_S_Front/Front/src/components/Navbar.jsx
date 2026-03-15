53477cf36745e26fd24e7e31073ddd1f37f90ff0
Mohamed03738jd <mohamedelaazzaoui00@gmail.com>
2026-03-15 13:17:08 +0000

    fix: cors + application.properties corrected

--- C_R_S_Front/Front/src/components/Navbar.jsx
+++ C_R_S_Front/Front/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        🏦 Gestion des Crédits Mohamed
+        🏦 Gestion des Crédits
       </div>
       
       <div className="navbar-menu">
