6694eaef12a64a64da8c7482f45640cdd7520a26
Mohamed03738jd <mohamedelaazzaoui00@gmail.com>
2026-03-24 22:42:29 +0100

    v.001

--- C_R_S_Front/Front/src/components/Navbar.jsx
+++ C_R_S_Front/Front/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        🏦 Gestion des Crédits
+        🏦 Gestion des Mohamed
       </div>
       
       <div className="navbar-menu">
